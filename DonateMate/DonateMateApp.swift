6c9dc25c6a7979e9e608688d106c4c0aa2d0de47
Jonathan Edwin <113654202+Bassman8K@users.noreply.github.com>
2025-02-24 10:33:37 +1100

    Initial Commit

---
+++ DonateMate/DonateMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonateMateApp.swift
+//  DonateMate
+//
+//  Created by Jonathan Edwin on 24/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct DonateMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
