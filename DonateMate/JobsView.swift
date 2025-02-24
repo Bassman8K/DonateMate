d99ba96230f67c60319577bbed3e5670f47d56f2
Sara <wlot31@gmail.com>
2025-02-25 00:26:40 +1100

    Created a Jobs page, testing lists (only couch works, won't allow me to add other items to the jobs YET)

--- DonateMate/JobsView.swift
+++ DonateMate/JobsView.swift
@@ -9,7 +9,65 @@ import SwiftUI
 
 struct JobsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack {
+                Color("lightPurple")
+                    .ignoresSafeArea()
+               
+                VStack {
+                    List {
+                        
+                        ForEach([
+                            ("couch", "Couch", "Building 10", "Building 12"),
+
+                        ], id: \.1) { image, name, pickup, dropoff in
+                            HStack {
+                                // Image Placeholder
+                                Image(image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 50, height: 50)
+                                    .padding(.trailing, 8)
+                                
+                                VStack(alignment: .leading, spacing: 4) {
+                                    Text(name)
+                                        .font(.headline)
+                                        .bold()
+                                        .underline()
+                                    
+                                    HStack {
+                                        VStack(alignment: .leading) {
+                                            Text("Pick up Location")
+                                                .font(.caption)
+                                                .bold()
+                                            Text(pickup)
+                                                .font(.caption)
+                                                .foregroundColor(.gray)
+                                            
+                                        }
+                                        Spacer()
+                                        VStack(alignment: .trailing) {
+                                            
+                                            
+                                            Text("Drop off Location")
+                                                .font(.caption)
+                                                .bold()
+                                            Text(dropoff)
+                                                .font(.caption)
+                                                .foregroundColor(.gray)
+                                        }
+                                    }
+                                }
+                            }
+                            .padding(.vertical, 8)
+                        }
+                    }
+                    .scrollContentBackground(.hidden)
+                }
+            }
+            .navigationTitle("Jobs")
+           
+        }
     }
 }
 
