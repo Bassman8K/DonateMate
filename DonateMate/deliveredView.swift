4a2e161bb1284c27a9a2f3b78e7bea1f1a1e4561
Sara <wlot31@gmail.com>
2025-02-24 13:18:15 +1100

    Added feedbackView and deliveredView

---
+++ DonateMate/deliveredView.swift
@@ -0,0 +1,18 @@
+//
+//  deliveredView.swift
+//  DonateMate
+//
+//  Created by Sara Al Fahdawi on 24/2/2025.
+//
+
+import SwiftUI
+
+struct deliveredView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    deliveredView()
+}
---
+++ DonateMate/feedbackView.swift
@@ -0,0 +1,18 @@
+//
+//  feedbackView.swift
+//  DonateMate
+//
+//  Created by Sara Al Fahdawi on 24/2/2025.
+//
+
+import SwiftUI
+
+struct feedbackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    feedbackView()
+}
