01bc6cc7b5f273a5295a23499c617cc591c387a0
Annie <s3978369@rmit.edu.vn>
2025-02-26 11:37:28 +1100

    UX/UI Of the IteamDetails

--- DonateMate/ItemDetails.swift
+++ DonateMate/ItemDetails.swift
@@ -51,59 +51,99 @@ struct ItemDetails: View {
             itemImage
                 .resizable()
                 .scaledToFit()
-                .frame(width: 245.48798, height: 249.19385)
-            VStack (alignment: .leading){
+                .frame(height: 249.19385)
+                .cornerRadius(5)
+            VStack (alignment: .leading, spacing: 10){
                 Color("lightPurple")
                     .ignoresSafeArea()
                 
-                
                 VStack (alignment: .leading, spacing: 20){
                     Text(itemName)
+                        .fontWeight(.heavy)
                         .font(.largeTitle)
+                        .foregroundColor(Color.darkPurple)
                     
-                    Text(itemDistance)
-                        .font(.title2)
-                    
+                    HStack {
+                        Image(systemName: "location.fill")
+                            .foregroundColor(Color.darkPurple)
+                        Text(itemDistance)
+                            .font(.title2)
+                            .foregroundColor(Color.darkPurple)
+                            .italic()
+                    }
+                    .frame(width: 100, height: 50)
+                    .background(Color.white)
+                    .cornerRadius(20)
+                    .shadow(radius: 2)
                     
                     Text("Description")
-                        .font(.title3)
+                        .font(.title2)
+                        .bold()
+                        .foregroundColor(Color.darkPurple)
                     Text(description)
+                        .opacity(0.5)
                     
                     
                     Text("Address")
-                        .font(.title3)
+                        .font(.title2)
+                        .bold()
+                        .foregroundColor(Color.darkPurple)
                     Text(address)
+                        .opacity(0.5)
                     
                     Text("Instructions")
-                        .font(.title3)
+                        .font(.title2)
+                        .bold()
+                        .foregroundColor(Color.darkPurple)
                     Text(instructions)
-                    
+                        .opacity(0.5)
                     
                     Text("Selected Dates:")
-                        .font(.title3)
+                        .font(.title2)
+                        .bold()
+                        .foregroundColor(Color.darkPurple)
                     
                     ForEach(Array(selectedDates), id: \.self) { dateComponent in
                         let date = Calendar.current.date(from: dateComponent)
                         Text(dateFormatter.string(from: date!))
+                            .opacity(0.5)
+                        
                     }
-                    VStack {
-                        Text("From:")
-                            .font(.title3)
+                    HStack {
+                        VStack{
+                            Text("From:")
+                                .font(.title3)
+                                .bold()
+                                .italic()
+                                .foregroundColor(Color.darkPurple)
+                            
+                            Text(timeFormatter.string(from: fromTime))
+                                .foregroundColor(Color.black)
+                                .opacity(0.5)
+                        }
+                        .padding(.horizontal, 20)
                         
-                        Text(timeFormatter.string(from: fromTime))
-                        
-                        Text("To:")
-                            .font(.title3)
-                        
-                        Text(timeFormatter.string(from: toTime))
+                        VStack{
+                            Text("To:")
+                                .font(.title3)
+                                .bold()
+                                .italic()
+                                .foregroundColor(Color.darkPurple)
+                            
+                            Text(timeFormatter.string(from: toTime))
+                                .foregroundColor(Color.black)
+                                .opacity(0.5)
+
+                        }
                     }
-                    .background(.white)
-                    .frame(width:300 , height: 100)
                 }
                 .padding()
                 
                 VStack (alignment: .center){
                     Text("Choose a recipient")
+                        .font(.title2)
+                        .bold()
+                        .foregroundColor(Color.darkPurple)
                     Picker("Choose a recipient", selection: $recipient) {
                         ForEach(recipients, id: \.self) { recipient in
                             Text(recipient)
@@ -120,21 +160,21 @@ struct ItemDetails: View {
                 //Spacer()
             }
             
-            // .padding()
-            
+            .padding(.horizontal, 20)
             
             .background(Color(red: 0.87, green: 0.87, blue: 1))
             
         }
+        .background(Color(red: 0.87, green: 0.87, blue: 1))
     }
     
 }
 
 #Preview {
     let exampleDateComponents = Set<DateComponents>([
-                DateComponents(year: 2025, month: 3, day: 1)
-            ])
-            
+        DateComponents(year: 2025, month: 3, day: 1)
+    ])
+    
     let exampleFromTime = Calendar.current.date(from: DateComponents(hour: 9, minute: 0))!
     let exampleToTime = Calendar.current.date(from: DateComponents(hour: 17, minute: 0))!
     ItemDetails(itemName: "Couch", itemImage: Image("couch"), itemDistance: "3km",description: "It has 50 stains", address: "123 Hello Town", instructions: "It will be on the porch", selectedDates: exampleDateComponents, fromTime: exampleFromTime, toTime: exampleToTime )
